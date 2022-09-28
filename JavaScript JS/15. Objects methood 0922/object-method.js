c8eb6725631c229188cfe3ee5e2eb20267af86d6
ArturV <volvset@gmail.com>
2022-09-28 23:32:33 +0300

    object update

--- JavaScript JS/15. Objects methood 0922/object-method.js
+++ JavaScript JS/15. Objects methood 0922/object-method.js
@@ -1,4 +1,4 @@
-const person = {
+const personX = {
   age: 25,
   salary: 1_000,
 };
@@ -8,33 +8,78 @@ const persons = [
   salary: parseInt(600+Math.random()*2_000),  // 600..2000 alga $
 ]
 */
-console.log(person.salary);
-console.log(persons);
+console.log(personX.salary);
+console.log(persoXn);
 
-const getPerson = () =>{
-  const randomNumber= Math.random();
-  const age=parseInt(20+randomNumber *10);
-  const salary= parseInt(600 + randomNumber *2_000);
- 
-  return  {
+const getPerson = () => {
+  const randomNumber = Math.random();
+
+  const age = parseInt(20 + randomNumber * 10);
+  const salary = parseInt(600 + randomNumber * 2_000);
+
+  const showWeekday = () => {
+    const date = new Date();
+    const weekday = date.toLocaleString("lt-LT", { weekday: "long" });
+
+    console.log(`Sveiki! Šiandien ${weekday}.`);
+  };
+
+  const getWeekday = () => {
+    const date = new Date();
+    const weekday = date.toLocaleString("lt-LT", { weekday: "long" });
+
+    return `Sveiki! Šiandien ${weekday}.`;
+  };
+
+  return {
     age,
     salary,
+    getWeekday,
+    showWeekday,
   };
+  /* 
+  return {
+    age: age,
+    salary: salary
+  }*/
 };
 
-console.log(getPerson());
+const person = getPerson();
 
-const getPersonsSalary = () => {
-  const persons [];
+// const showWeekday = person.showWeekday;
 
-  for (let i=0; i<15; i++){
+console.log(
+  `${person.getWeekday()} Amžius: ${person.age}, alga: ${person.salary}`
+);
+console.log(person);
+
+const getSalaryPerAge = () => {
+  const persons = [];
+
+  for (let i = 0; i < 15; i++) {
     const person = getPerson();
 
-    person.push(person);
+    persons.push(person);
   }
-  const totalAge = person.reduce((previousValue, currenValue)=> previousValue + currenValue.age,0) ;
-  const totalSalary = person.reduce((previousValue, currenValue)=> previousValue + currenValue.salary,0);
 
-  return totalSalary/totalAge;
+  const totalAge = persons.reduce(
+    (previousValue, currentValue) => previousValue + currentValue.age,
+    0
+  );
+
+  const totalSalary = persons.reduce(
+    (previousValue, currentValue) => previousValue + currentValue.salary,
+    0
+  );
+
+  return (totalSalary / totalAge).toFixed(2);
 };
 
+const salaryDividedByAge = getSalaryPerAge();
+
+// !!!
+const getAge = () => 25;
+
+const getMyAge = () => {
+  return 25;
+};
