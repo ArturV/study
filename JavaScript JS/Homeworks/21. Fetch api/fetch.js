39010aca1a3b6d5be540142cb8dd2a02a32259cb
ArturV <volvset@gmail.com>
2022-10-24 00:13:53 +0300

    21 home

--- JavaScript JS/Homeworks/21. Fetch api/fetch.js
+++ JavaScript JS/Homeworks/21. Fetch api/fetch.js
@@ -1,34 +1,39 @@
 // Jums paskambino pažinčių portalas – jiems reikia greitai sukurti front-endą, kuris pasiimtų duomenis
-// iš https://randomuser.me/api/ ir juos atvaizduotų juos puslapyje. Duomenys galėtų būti atvaizduoti panašioje kortelėje.
+// iš https://randomuser.me/api/ ir juos atvaizduotų juos puslapyje. Duomenys galėtų būti atvaizduoti kortelėje.
 // Svarbu atvaizduoti nuotrauką, vardą, amžių ir el. pašto adresą.
 
-const getData = async () => {
-  const getUsers = async () => {
-    const response = await fetch("https://randomuser.me/api/");
+const getUsers = async () => {
+  try {
+    const response = await fetch("https://randomuser.me/api");
     const users = await response.json();
 
     console.log(users);
+    document.getElementById("container").innerHTML = JSON.stringify(users);
     return users;
-  };
-
-  //const users = await getUsers();
-
-  //console.log(users);
-
-  const renderUser = async () => {
-    const users = getUsers();
-    let html = "";
-
-    users.forEach((user) => {
-      let htmlInsertion = `<img src="${user.picture.large}" alt="alt"/> <p>Email: ${user.email}</p>`;
-      html += htmlInsertion;
-    });
-
-    let container = document.querySelector(".container");
-    container.innerHTML = html;
-  };
+  } catch (error) {
+    console.error(error);
+  }
 };
 
-getData();
+const renderUser = (users) => {
+  const email = document.createElement("p");
+  const image = document.createElement("img");
 
-//renderUser();
+  users.forEach((element) => {
+    email.textContent = element.email;
+
+    image.src = users.picture.large;
+    image.setAttribute("alt", "UserPicture");
+
+    const email2 = document.createElement("p");
+    email2.textContent = element.email2;
+
+    const addImage = document.body.append(image);
+    const addEmail = document.body.append(email);
+    document.body.append(email2);
+  });
+};
+
+//const userX = getUsers();
+getUsers();
+renderUser();
