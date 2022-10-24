9edf50f6c85d78ffaac6650f355281c2844bcac0
ArturV <volvset@gmail.com>
2022-10-24 19:59:54 +0300

    database practise update

---
+++ JavaScript JS/26. databaseProject/modules/updatePost.js
@@ -0,0 +1,7 @@
+const updatePost = () => {
+  const newBodyValue = document.querySelector("#newBodyInput").value.trim(); // trim - istrina tarpus kaireje ir desineje
+
+  console.log(newBodyValue);
+};
+
+export { updatePost };
