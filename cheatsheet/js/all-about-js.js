25a3543a3b57064d4f3368f977acdc68fb6b0352
ArturV <volvset@gmail.com>
2022-10-28 19:29:23 +0300

    oop constructor es5 es6

--- cheatsheet/js/all-about-js.js
+++ cheatsheet/js/all-about-js.js
@@ -708,7 +708,7 @@ const products = [
 
 console.log(getProductsSum(products));
 
-// ------------------------ OOP ------------------------- //
+// ------------------------ OOP classes, constructors------------------------- //
 
 function Phone(hardware, processor, screenSize) {
   /// function Consructor
@@ -766,6 +766,187 @@ const shirt = new Product(50, 150);
 
 console.log(shirt.unitsAvailable);
 console.log(shirt.getSumRevenue());
+
+// Classes in ES5
+
+/**
+ * Person class.
+ *
+ * @constructor
+ * @param {String} name - name of a person.
+ * @param {Number} age  - age of a person.
+ * @param {String} gender  - gender of a person.
+ */
+
+function Person(name, age, gender) {
+  this.name = name;
+  this.age = age;
+  this.gender = gender;
+}
+
+Person.prototype.getName = function () {
+  return this.name;
+};
+
+Person.prototype.getAge = function () {
+  return this.age;
+};
+
+Person.prototype.getGender = function () {
+  return this.gender;
+};
+
+/**
+ * Teacher class.
+ *
+ * @constructor
+ * @param {String} name - name of a teacher.
+ * @param {Number} age  - age of a teacher.
+ * @param {String} gender  - gender of a teacher.
+ * @param {String} subject - subject of a teacher.
+ */
+
+function Teacher(name, age, gender, subject) {
+  Person.call(this, name, age, gender);
+  this.subject = subject;
+}
+
+Teacher.prototype = Object.create(Person.prototype);
+
+Teacher.prototype.getSubject = function () {
+  return this.subject;
+};
+
+/**
+ * Student class.
+ *
+ * @constructor
+ * @param {String} name - name of a student.
+ * @param {Number} age  - age of a student.
+ * @param {String} gender  - gender of a student.
+ * @param {Number} marks - marks of a student.
+ */
+
+function Student(name, age, gender, marks) {
+  Person.call(this, name, age, gender);
+  this.marks = marks;
+}
+
+Student.prototype = Object.create(Person.prototype);
+
+Student.prototype.getMarks = function () {
+  return this.marks;
+};
+
+const teacher2 = new Teacher("John Doe", 30, "male", "Maths");
+const student2 = new Student("Jane Miles", 12, "female", 88);
+
+console.log(
+  "Teacher:",
+  teacher2.getName(),
+  teacher2.getAge(),
+  teacher2.getGender(),
+  teacher2.getSubject()
+);
+console.log(
+  "Student:",
+  student2.getName(),
+  student2.getAge(),
+  student2.getGender(),
+  student2.getMarks()
+);
+
+// classes in ES6
+
+/**
+ * Person class.
+ *
+ * @constructor
+ * @param {String} name - name of a person.
+ * @param {Number} age  - age of a person.
+ * @param {String} gender  - gender of a person.
+ */
+
+class Person {
+  constructor(name, age, gender) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
+
+  getName() {
+    return this.name;
+  }
+
+  getAge() {
+    return this.age;
+  }
+
+  getGender() {
+    return this.gender;
+  }
+}
+
+/**
+ * Teacher class.
+ *
+ * @constructor
+ * @param {String} name - name of a teacher.
+ * @param {Number} age  - age of a teacher.
+ * @param {String} gender  - gender of a teacher.
+ * @param {String} subject - subject of a teacher.
+ */
+
+class Teacher extends Person {
+  constructor(name, age, gender, subject) {
+    super(name, age, gender);
+
+    this.subject = subject;
+  }
+
+  getSubject() {
+    return this.subject;
+  }
+}
+
+/**
+ * Student class.
+ *
+ * @constructor
+ * @param {String} name - name of a student.
+ * @param {Number} age  - age of a student.
+ * @param {String} gender  - gender of a student.
+ * @param {Number} marks - marks of a student.
+ */
+
+class Student extends Person {
+  constructor(name, age, gender, marks) {
+    super(name, age, gender);
+    this.marks = marks;
+  }
+
+  getMarks() {
+    return this.marks;
+  }
+}
+
+const teacher = new Teacher("John Doe", 30, "male", "Maths");
+const student = new Student("Jane Miles", 12, "female", 88);
+
+console.log(
+  "Teacher:",
+  teacher.getName(),
+  teacher.getAge(),
+  teacher.getGender(),
+  teacher.getSubject()
+);
+console.log(
+  "Student:",
+  student.getName(),
+  student.getAge(),
+  student.getGender(),
+  student.getMarks()
+);
 
 // --------------------- COOKIES & local storage ------------------ //
 
