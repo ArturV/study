557eb294b14a150b8e4ae1b0c1faab49697fd723
ArturV <volvset@gmail.com>
2022-11-08 19:00:28 +0200

    post express

--- NODE JS/3. Express/index.js
+++ NODE JS/3. Express/index.js
@@ -5,11 +5,18 @@ const PORT = 5_000;
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
+app.get("/", (_, res) => {
+  // _ pries kableli reiskia kintamo nenaudojam, nors privalome islaikyti eiliskuma
   res.send({ age: 24 });
 });
 
 app.post("/", (req, res) => {
+  console.log(req.params);
+
+  res.send("0");
+});
+
+app.post("/:userId", (req, res) => {
   res.send({ age: req.body.age * 2 });
 });
 
