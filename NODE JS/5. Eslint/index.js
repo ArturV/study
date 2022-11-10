667e77d2a0b6ef68c92c3cc214292859e3bfb0c1
ArturV <volvset@gmail.com>
2022-11-10 18:08:30 +0200

    Eslint

---
+++ NODE JS/5. Eslint/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+
+/*
+npm init @eslint/config  - instaluojame Eslint'a
+npx eslint .  - paleidziame Eslint'a
+*/
+const app = express();
+
+const PORT = 5_000;
+
+app.use(express.json());
+
+app.get("/", (_, res) => {
+	// _ pries kableli reiskia kintamo nenaudojam, nors privalome islaikyti eiliskuma
+	res.send({ age: 24 });
+});
+
+app.post("/", (req, res) => {
+	console.log(req.params);
+
+	res.send("0");
+});
+
+app.post("/:userId", (req, res) => {
+	res.send({ age: req.body.age * 2 });
+});
+
+app.listen(PORT, () => {
+	console.log(`server is runnig on port: ${PORT}`);
+});
