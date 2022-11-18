dc2f478cc66a2d30d25cef410d1ff851ee23804b
ArturV <volvset@gmail.com>
2022-11-18 11:48:30 +0200

    mongodb

--- NODE JS/9.MongoDB-express/index.js
+++ NODE JS/9.MongoDB-express/index.js
@@ -1,15 +1,23 @@
 const express = require("express");
-
+const { MongoClient } = require("mongodb");
 require("dotenv").config(); // npm i dotenv pries tai irasome i terminala
 
 const app = express();
 const PORT = +process.env.PORT || 5000;
 const URI = process.env.URI;
+const client = new MongoClient(URI);
 
 app.use(express.json());
 
-app.post("/", (req, res) => {
-  res.send({ message: "Welcome to my project" });
+app.get("/", async (_, res) => {
+  const connection = await client.connect();
+  const data = await connection
+    .db("node-mongo-first-project")
+    .collection("users")
+    .find()
+    .toArray();
+  await connection.close();
+  return res.send(data);
 });
 
 app.listen(PORT, () => {
