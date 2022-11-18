d08ca314beda40d10293c695a76895b1bb9852ac
ArturV <volvset@gmail.com>
2022-11-18 11:58:59 +0200

    mongodb express

--- NODE JS/9.MongoDB-express/index.js
+++ NODE JS/9.MongoDB-express/index.js
@@ -10,14 +10,32 @@ const client = new MongoClient(URI);
 app.use(express.json());
 
 app.get("/", async (_, res) => {
-  const connection = await client.connect();
-  const data = await connection
-    .db("node-mongo-first-project")
-    .collection("users")
-    .find()
-    .toArray();
-  await connection.close();
-  return res.send(data);
+  try {
+    const connection = await client.connect();
+    const data = await connection
+      .db("node-mongo-first-project")
+      .collection("users")
+      .find()
+      .toArray();
+    await connection.close();
+    return res.send(data);
+  } catch (err) {
+    res.status(500).send({ err });
+  }
+});
+
+app.post("/", async (req, res) => {
+  try {
+    const con = await client.connect();
+    const dbRes = await con
+      .db("node-mongo-first-project")
+      .collection("users")
+      .insertOne({ name: "Petras", surname: "Slekys" });
+    await con.close();
+    return res.send(dbRes);
+  } catch (err) {
+    res.status(500).send({ err });
+  }
 });
 
 app.listen(PORT, () => {
