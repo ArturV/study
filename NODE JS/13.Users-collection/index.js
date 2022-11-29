866d102c324c311a8210de3edbaf8d1f5b0d4340
ArturV <volvset@gmail.com>
2022-11-29 20:41:07 +0200

    mongodb match group sort practise

--- NODE JS/13.Users-collection/index.js
+++ NODE JS/13.Users-collection/index.js
@@ -4,6 +4,12 @@ const { MongoClient, ObjectId } = require("mongodb");
 const app = express();
 require("dotenv").config();
 
+/*
+Užduotis: `users` kolekcijoje nustatyti unikalių vardų sąrašą (bent dešimt įrašų lentelėje).
+ Nustatyti, kiek įrašų turi vardą X (kur X - pateiktas per užklausą. Apmąstykit, body ar params).
+Nustatyti bendrą vartotojo vardu X užsakymų kiekį
+*/
+
 const URI = process.env.URI;
 const client = new MongoClient(URI);
 const PORT = +process.env.PORT || 5000;
@@ -15,7 +21,17 @@ app.use(express.json());
 
 app.get("/users/:name", async (req, res) => {
   const { name } = req.params;
-  console.log(name);
+
+  const pipeline = [
+    { $match: { name } },
+    { $group: { _id: "$name", orders: { $sum: "$orders" } } },
+    { $sort: { orders: -1 } },
+  ];
+
+  if (typeof name !== "string") {
+    res.status(400).send("name is not a string.").end();
+    return;
+  }
 
   if (!name) {
     res.status(400).send("This name was not provided.").end();
@@ -24,15 +40,36 @@ app.get("/users/:name", async (req, res) => {
 
   try {
     const connection = await client.connect();
+    const docs = [];
+
+    const entries = await connection
+      .db(DB)
+      .collection(DBCOLLECTION)
+      .distinct("name");
+
+    const nameCount = await connection
+      .db(DB)
+      .collection(DBCOLLECTION)
+      .count({ name });
+
     const data = await connection
       .db(DB)
       .collection(DBCOLLECTION)
-      .find({ name: { $regex: name } })
+      .find({ name })
       .toArray();
-    console.log(data);
+
+    const aggregationCursor = await connection
+      .db(DB)
+      .collection(DBCOLLECTION)
+      .aggregate(pipeline);
+
+    for await (const doc of aggregationCursor) {
+      docs.push(doc);
+    }
+
     await connection.close();
 
-    return res.send(data).end();
+    res.send({ nameCount, docs, data, entries }).end();
   } catch (err) {
     res.status(500).send({ err }).end();
     throw Error(err);
