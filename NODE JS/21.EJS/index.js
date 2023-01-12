afd7601d5596fd01891eb0ff17c0fe5761d570e8
ArturV <volvset@gmail.com>
2023-01-12 18:33:02 +0200

    EJS

---
+++ NODE JS/21.EJS/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import { renderHome } from "./modules/renderHome.js";
+
+const app = express();
+const PORT = 5_000;
+
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+
+app.get("/", renderHome);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port: ${PORT}`);
+});
