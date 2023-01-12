ce12e536f8b3b1675679fa71063fd4593046a69f
ArturV <volvset@gmail.com>
2023-01-12 20:58:42 +0200

    EJS example

--- NODE JS/21.EJS/modules/renderHome.js
+++ NODE JS/21.EJS/modules/renderHome.js
@@ -1,3 +1,15 @@
 export const renderHome = (req, res) => {
-  res.send("Welcome").end();
+  const number = +req.query.number; // i url irasome: pvz localhost:5000/?number=5
+  const countdownNumber = +req.query.countdownNumber;
+
+  const ip =
+    req.ip || req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+
+  res.render("index", {
+    title: "codeAcademy",
+    pageName: "Home",
+    ip,
+    number,
+    countdownNumber,
+  });
 };
